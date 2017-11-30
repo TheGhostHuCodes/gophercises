ef9eb45e4011e2b4737851f43a9b9e79b4dad664
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-11-30 07:21:55 -0800

    Add input file commandline parsing

--- exercise04/main.go
+++ exercise04/main.go
@@ -2,22 +2,25 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"log"
+	"os"
 
 	"github.com/TheGhostHuCodes/gophercises/exercise04/link"
 )
 
-var exampleHTML = `
-<html>
-<body>
-  <h1>Hello!</h1>
-  <a href="/other-page">A link to another page</a>
-</body>
-</html>
-`
-
 func main() {
-	r := strings.NewReader(exampleHTML)
+	args := os.Args[1:]
+	if len(args) != 1 {
+		fmt.Printf("Recieved %d arguments (%v), expected 1 argument\n", len(args), args)
+		os.Exit(1)
+	}
+
+	filename := args[0]
+	r, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("Unable to open file %s, received error: %v", args[0], err)
+	}
+
 	links, err := link.Parse(r)
 	if err != nil {
 		panic(err)
